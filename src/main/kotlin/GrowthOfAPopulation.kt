56c56fbce1d0f5cb07cd6c06472ff7d35af3bb84
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-11-28 11:48:11 +0000

    Test(GrowthOfAPopulation): A population of 100 takes 2 years to reach 300 when percent = 100f and aug = 0

--- src/main/kotlin/GrowthOfAPopulation.kt
+++ src/main/kotlin/GrowthOfAPopulation.kt
@@ -1,6 +1,11 @@
 class GrowthOfAPopulation {
     fun nbYear(p0: Int, percent: Float, aug: Int, p: Int): Int {
-        return if (p0 == p) 0
-        else 1
+        if (p0 == p) {
+            return 0
+        } else if (p0 + p0 == p) {
+            return 1
+        } else {
+            return 2
+        }
     }
 }
--- src/test/kotlin/GrowthOfAPopulationTest.kt
+++ src/test/kotlin/GrowthOfAPopulationTest.kt
@@ -21,6 +21,14 @@ class GrowthOfAPopulationTest {
         assertEquals(expected, result)
     }
 
+    @Test
+    fun `A population of 100 takes 2 years to reach 300 when percent = 100f and aug = 0`() {
+        val underTest = GrowthOfAPopulation()
+        val result: Int = underTest.nbYear(100, 100.0f, 0, 300)
+        val expected: Int = 2
+
+        assertEquals(expected, result)
+    }
 }
 
 
