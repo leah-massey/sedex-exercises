0004fd16502c3839b8648ff3613c6d52e0642064
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-04 09:53:01 +0000

    Test(WrappingPaper): a square box 1ft x 2ft x 3ft returns 27

--- src/main/kotlin/WrappingPaper.kt
+++ src/main/kotlin/WrappingPaper.kt
@@ -1,6 +1,11 @@
 class WrappingPaper {
     fun paperNeeded(l: Int, w: Int, h: Int): Int {
-        val area = 2*l*w + 2*w*h + 2*h*l + 1
+
+        val sides: List<Int> = listOf(l*w, w*h, h*l).sorted()
+        var area = 0
+        sides.forEach { area += 2*it}
+        area += sides[0]
+
         return area
     }
 }
--- src/test/kotlin/WrappingPaperTest.kt
+++ src/test/kotlin/WrappingPaperTest.kt
@@ -4,12 +4,23 @@ import org.junit.jupiter.api.Test
 class WrappingPaperTest{
     @Test
     fun `a square box 1ft x 1ft x 1ft returns 7`() {
-        // 2*1 + 2*1+ 2*1 + 1
-        //7 sq feet
+        // 2*1 + 2*1+ 2*1 + 1  = 7 sq feet
 
         val underTest = WrappingPaper()
         val expected = 7
         val actual = underTest.paperNeeded(1, 1, 1)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `a square box 1ft x 2ft x 3ft returns 27`() {
+        // 2*1*2 + 2*2*3+ 2*3*1 + 2  =  ? sq feet
+        // 4 + 12 + 6 + 2 = 24
+
+        val underTest = WrappingPaper()
+        val expected = 24
+        val actual = underTest.paperNeeded(1, 2, 3)
 
         assertEquals(expected, actual)
     }
