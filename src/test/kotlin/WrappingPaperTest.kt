6e40c1ec7a5d737bb84e9e1d933131a285a26411
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-04 09:56:26 +0000

    Test(WrappingPaper): a box 2ft x 3ft x 4ft returns 58

--- src/test/kotlin/WrappingPaperTest.kt
+++ src/test/kotlin/WrappingPaperTest.kt
@@ -14,13 +14,25 @@ class WrappingPaperTest{
     }
 
     @Test
-    fun `a square box 1ft x 2ft x 3ft returns 27`() {
+    fun `a box 1ft x 2ft x 3ft returns 27`() {
         // 2*1*2 + 2*2*3+ 2*3*1 + 2  =  ? sq feet
         // 4 + 12 + 6 + 2 = 24
 
         val underTest = WrappingPaper()
         val expected = 24
         val actual = underTest.paperNeeded(1, 2, 3)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `a box 2ft x 3ft x 4ft returns 58`() {
+        // 2*1*2 + 2*2*3+ 2*3*1 + 2  =  ? sq feet
+        // 4 + 12 + 6 + 2 = 24
+
+        val underTest = WrappingPaper()
+        val expected = 58
+        val actual = underTest.paperNeeded(2, 3, 4)
 
         assertEquals(expected, actual)
     }
