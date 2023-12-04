797ecb34ec84b09b58ea2420a4998bebdc5c280d
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-04 15:01:39 +0000

    Test(WrappingPaperTest): refactor

--- src/main/kotlin/WrappingPaper.kt
+++ src/main/kotlin/WrappingPaper.kt
@@ -1,30 +1,16 @@
 class WrappingPaper {
-//    fun paperNeeded(l: Int, w: Int, h: Int): Int {
-//
-//        val sides: List<Int> = listOf(l*w, w*h, h*l)
-//        sides.map { (a, b) -> }
-//
-//    }
-
-
     fun paperNeeded(allBoxes: List<String>): Int {
 
         var totalPaper = 0
 
         allBoxes.map {
-
             val boxDimensions = it.split("x").map{it.toInt()}
-
             totalPaper += paperForBox(boxDimensions)
-
         }
         return totalPaper
     }
 
-
     // this function finds the area for each box
-
-//    fun paperForBox(l: Int, w: Int, h: Int): Int {
     fun paperForBox(dimensions: List<Int>): Int {
         val (l, w, h) = dimensions
 
