3cd4fd91467c21ae7783e0fcccc5581733190e2b
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-05 12:53:29 +0000

    Test(LostSanta): returns the position of the first character that causes him to enter the basement

--- src/main/kotlin/LostSanta.kt
+++ src/main/kotlin/LostSanta.kt
@@ -1,15 +1,67 @@
-class LostSanta {
-    fun findFloor(floorMap: String): Int {
-        var instructions: List<Char> = floorMap.toList()
-        var floor: Int = 0
+class LostSanta(floorMap: String) {
 
+    val instructions: List<Char> = floorMap.toList()
+    var floor: Int = 0
+    var position: Int = 0
+
+    fun findFloor(): Int {
         instructions.forEach {
-            if(it == '(') {
-            floor++
-            } else {
-            floor--
+            when(it) {
+                '(' -> floor++
+                ')' -> floor--
             }
         }
         return floor
     }
+
+    fun hitsBasement():String {
+        instructions.forEach {
+            when(it) {
+                '(' -> floor++
+                ')' -> floor--
+            }
+            position ++
+            if (floor == - 1) {
+                return position.toString()
+
+            }
+        }
+        return "you never go in the basement"
+    }
 }
+
+//class LostSanta {
+//    fun findFloor(floorMap: String): Int {
+//        var instructions: List<Char> = floorMap.toList()
+//        var floor: Int = 0
+//
+//        instructions.forEach {
+//            when(it) {
+//                '(' -> floor++
+//                ')' -> floor--
+//            }
+//        }
+//        return floor
+//    }
+//
+//    fun hitsBasement(floorMap: String):Int {
+//        var instructions: List<Char> = floorMap.toList()
+//        var floor: Int = 0
+//        var position = 0
+//
+//        instructions.forEach {
+//            when(it) {
+//                '(' -> floor++
+//                ')' -> floor--
+//            }
+//
+//            position ++
+//
+//            if (floor == - 1) {
+//                return position
+//            }
+//        }
+//
+//        return floor
+//    }
+//}
