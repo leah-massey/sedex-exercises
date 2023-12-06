f92b3eca76b925c95bb1ddc45d69009be5b43205
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-06 12:02:08 +0000

    Test(VowelCount): refactor using .count function

--- src/main/kotlin/VowelCount.kt
+++ src/main/kotlin/VowelCount.kt
@@ -4,15 +4,7 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.*/
 
 class VowelCount {
-
     fun count(string: String): Int {
-        var vowelsInString: Int = 0
-        string.split("").map {
-            if (it in listOf("a", "e", "i", "o", "u")) {
-                vowelsInString += 1
-            }
-        }
-        return vowelsInString
+        return string.count {it in "aeiou"}
     }
-
 }
