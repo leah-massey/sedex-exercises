1d043972522ef378f5bb7fa2f9fac3991b9edfcd
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-06 17:16:10 +0000

    Test(RomanNumerals): an input 1000 returns "M"

---
+++ src/main/kotlin/RomanNumerals.kt
@@ -0,0 +1,5 @@
+class RomanNumerals {
+    fun findRomNum(number: Int): String {
+        return "M"
+    }
+}
---
+++ src/test/kotlin/RomanNumeralsTest.kt
@@ -0,0 +1,21 @@
+/*Create a function taking a positive integer between 1 and 3999 (both included) as its parameter and returning
+a string containing the Roman Numeral representation of that integer.
+
+Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping
+any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+Example:
+
+encode(1000) // should return "M" */
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class RomanNumeralsTest {
+    @Test
+    fun `an input 1000 returns "M"`() {
+        val underTest = RomanNumerals()
+        val expected = "M"
+        val actual = underTest.findRomNum(1000)
+    }
+}
