963a357d7bb886a60e299bcfb14c524d7019cca0
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-06 15:14:07 +0000

    Test(AlphabeticalSequence): refactor

--- src/main/kotlin/AlphabeticalSequence.kt
+++ src/main/kotlin/AlphabeticalSequence.kt
@@ -1,33 +1,10 @@
 import java.lang.Character.UnicodeScript
 
 class AlphabeticalSequence {
-    fun alphaSeq(string: String): String {
-
-        var chars: CharArray = string.lowercase().toCharArray()
-        chars.sort()
-
-        val finalSeq: MutableList<String> = mutableListOf()
-
-//      for (char in chars) {
-//
-//            val firstLetter: Char = char.uppercaseChar()
-//            val letterCount: Int = char.code - 97
-//            val letterMultiples: String = char.toString().repeat(letterCount)
-//
-//            val completedString: String = "$firstLetter$letterMultiples"
-//            finalSeq.add(completedString)
-//        }
-
-        chars.forEach {
-//
-            val firstLetter: Char = it.uppercaseChar()
-            val letterCount: Int = it.code - 97
-            val letterMultiples: String = it.toString().repeat(letterCount)
-
-            val completedString: String = "$firstLetter$letterMultiples"
-            finalSeq.add(completedString)
-
-        }
-        return finalSeq.joinToString(",")
+    fun alphaSeq(str: String): String {
+        return str.lowercase().map {
+            it.toString().repeat(it.code - 96)
+                .replaceFirstChar { it.uppercaseChar() }
+        }.sorted().joinToString(",")
     }
 }
