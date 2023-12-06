f585d7b302e814a00cbd81705438b8d7161096a9
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-06 16:24:16 +0000

    Test(Exclamation): it turns a vowel to an exclamation in a word

--- src/main/kotlin/Exclamation.kt
+++ src/main/kotlin/Exclamation.kt
@@ -1,5 +1,18 @@
 class Exclamation {
     fun vowelToEx(sentence: String): String {
-        return "!"
+
+        var updatedSentence: MutableList<Char> = mutableListOf()
+
+        sentence.forEach{
+            if (it in "aeiou") {
+                updatedSentence.add('!')
+            } else {
+                updatedSentence.add(it)
+            }
+        }
+
+        return updatedSentence.joinToString("")
+
+
     }
 }
--- src/test/kotlin/ExclamationTest.kt
+++ src/test/kotlin/ExclamationTest.kt
@@ -3,9 +3,20 @@ import org.junit.jupiter.api.Test
 
 class ExclamationTest {
     @Test
-    fun `it turns a vowel to an exclamation`() {
+    fun `it turns a vowel to an exclamation in a single letter sentence`() {
         val underTest = Exclamation()
         val expected  = "!"
         val result = underTest.vowelToEx("a")
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `it turns a vowel to an exclamation in a word`() {
+        val underTest = Exclamation()
+        val expected  = "H!!"
+        val result = underTest.vowelToEx("Hi!")
+
+        assertEquals(expected, result)
     }
 }
