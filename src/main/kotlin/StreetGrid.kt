d77d92724894bf6b14263ca51d5946ce1adea7f9
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-07 15:39:06 +0000

    Test(StreetGrid):a movement in two in a single direction takes you 2 blocks away

--- src/main/kotlin/StreetGrid.kt
+++ src/main/kotlin/StreetGrid.kt
@@ -1,5 +1,18 @@
 class StreetGrid {
     fun blockDistance(directions: List<String>): Int {
-        return 1
+
+        var distance: Int = 0
+
+        println(directions)
+
+        directions.forEach {      // listOf("L2") it = "L2" -> ['L' '2']
+
+            val movement: Int = it.toCharArray()[1].digitToInt() // get number
+
+            distance += movement
+            val direction = it.toList()[0]
+
+        }
+        return distance
     }
 }
--- src/test/kotlin/StreetGridTest.kt
+++ src/test/kotlin/StreetGridTest.kt
@@ -3,9 +3,20 @@ import org.junit.jupiter.api.Test
 
 class StreetGridTest {
     @Test
-    fun `a single movement in any direction leaves you 1 block away`() {
+    fun `a movement of 1 in any direction leaves you 1 block away`() {
         val underTest = StreetGrid()
         val expected = 1
         val actual = underTest.blockDistance(listOf("R1"))
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `a movement in two in a single direction takes you 2 blocks away`() {
+        val underTest = StreetGrid()
+        val expected = 2
+        val actual = underTest.blockDistance(listOf("L2"))
+
+        assertEquals(expected, actual)
     }
 }
