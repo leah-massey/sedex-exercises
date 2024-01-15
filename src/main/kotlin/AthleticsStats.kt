7f749223c5026cc771e4b2c2efa5fd401b832efb
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-01-15 19:48:57 +0000

    Test(AthleticsStats-resultsInSecs): when multiple results are in the list, the range is the difference between the fastest and slowest

--- src/main/kotlin/AthleticsStats.kt
+++ src/main/kotlin/AthleticsStats.kt
@@ -1,16 +1,38 @@
-class AthleticsStats(results: String) {
-    val results = results //"00|00|20"
-    fun resultsInSecs(): List<Int> {
-        val hoursMinsSecs: List<String> = results.split("|")
-        println(hoursMinsSecs)
-        val hoursAsSecs: Int = hoursMinsSecs[0].toInt()*3600
-        val minsAsSecs: Int = hoursMinsSecs[1].toInt()*60
-        val secs: Int = hoursMinsSecs[2].toInt()
+import kotlin.math.min
 
-        return listOf(hoursAsSecs + minsAsSecs + secs)
+class AthleticsStats(val results: String) {
+    fun resultsInSecs(): List<Int> {
+
+        val splitResults: List<String> = results.split(", ")
+
+        val timeParts: List<List<String>> = splitResults.map { hhmmss ->
+             hhmmss.split("|")
+        }
+
+        val totalSecondsCalculated: List<Int> = timeParts.map { time ->
+            val hours = time[0].toInt()*3600
+            val mins = time[1].toInt()*60
+            val secs = time[2].toInt()
+            hours+ mins + secs
+        }
+
+    return totalSecondsCalculated
+
+    }
+
+    init {
+
     }
 
     fun range(): String {
-        return "00|00|00"
+ val sortedResultsInSecs = resultsInSecs().sorted()
+        val timeRangeTotalInSecs  = sortedResultsInSecs.last() - sortedResultsInSecs.first()
+
+        val timeRangeHours = (timeRangeTotalInSecs / 3600)
+        val timeRangeMins = (timeRangeTotalInSecs % 3600) / 60
+        val timeRangeSecs = (timeRangeTotalInSecs % 3600) % 60
+
+
+        return "${timeRangeHours.toString().padStart(2,'0')}|${timeRangeMins.toString().padStart(2,'0')}|${timeRangeSecs.toString().padStart(2,'0')}"
     }
 }
--- src/test/kotlin/AthleticsStatsTest.kt
+++ src/test/kotlin/AthleticsStatsTest.kt
@@ -13,6 +13,13 @@ class AthleticsStatsTest {
             val actual = underTest.resultsInSecs()
 
             assertEquals(expected, actual)
+        }@Test
+        fun `results are converted from hh|mm|ss to ss and retuned as list`() {
+            val underTest = AthleticsStats("02|20|10, 01|01|01")
+            val expected = listOf(8410, 3661) // 7200 + 1200 + 10
+            val actual = underTest.resultsInSecs()
+
+            assertEquals(expected, actual)
         }
     }
 
@@ -21,10 +28,21 @@ class AthleticsStatsTest {
         @Test
          fun `a single entry returns a range 0`() {
             val underTest = AthleticsStats("00|20|10")
-            val expected = 0
+            val expected = "00|00|00"
             val actual = underTest.range()
 
+            assertEquals(expected, actual)
          }
+
+        @Test
+        fun `when multiple results are in the list, the range is the difference between the fastest and slowest`() {
+            val underTest = AthleticsStats("00|20|10, 00|21|10")
+            val expected = "00|01|00"
+            val actual = underTest.range()
+
+            assertEquals(expected, actual)
+
+        }
 
     }
 }
