111a02cea655c986c41c6698ec7ad140d90a2e2f
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-11-23 20:49:02 +0000

    Test(Find): an array of 2 odd integers and one even returns the even integer (hardcoded)

---
+++ src/test/kotlin/FindTest.kt
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+
+//You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+// The array is either entirely comprised of odd integers or entirely
+// comprised of even integers except for a single integer N.
+// Write a method that takes the array as an argument and returns this "outlier" N.
+
+
+class FindTest() {
+    @Test
+    fun `an array of 2 odd integers and one even returns the even integer`() {
+        //given
+        val integers = listOf(1,2,3)
+        val undertest = Find()
+        //when
+        val actual = undertest.find(integers)
+        //then
+        assertEquals(2, actual)
+    }
+}
