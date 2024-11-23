eb6f7f0bd68384a8b67bb9bd4fcaa69becc72364
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-11-23 21:26:47 +0000

    Test(Find): refactor

--- src/main/kotlin/Find.kt
+++ src/main/kotlin/Find.kt
@@ -1,7 +1,7 @@
 class Find {
     fun find(numberArray: Array<Int>): Int {
-        val splitOddAndEven = numberArray.partition { number -> number % 2 == 0 }
-        return if (splitOddAndEven.first.size == 1)  splitOddAndEven.first[0] else  splitOddAndEven.second[0]
+        val (even, odd) = numberArray.partition { number -> number % 2 == 0 }
+        return if (even.size == 1)  even[0] else  odd[0]
     }
 }
 
