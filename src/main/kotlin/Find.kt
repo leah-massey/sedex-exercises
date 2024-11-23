5760397ff04254310435267bfc76e546f010eb94
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-11-23 21:14:31 +0000

    Test(Find): an array of 2 odd integers and one even returns the even integer

--- src/main/kotlin/Find.kt
+++ src/main/kotlin/Find.kt
@@ -1,5 +1,13 @@
 class Find {
     fun find(list: List<Int>): Int {
-        return 2
+        val splitOddAndEven: Pair<List<Int>, List<Int>> = list.partition { number -> number % 2 == 0 }
+
+        if (splitOddAndEven.first.size == 1) {
+            return splitOddAndEven.first[0]
+        } else {
+            return splitOddAndEven.second[0]
+        }
     }
 }
+
+
